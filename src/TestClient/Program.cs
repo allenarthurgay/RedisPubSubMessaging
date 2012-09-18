82f5eed01604774e11185d4ee8dd1b92b53e7392
dburlingame <dburlingame@daptiv.com>
2012-09-18 11:01:52 -0700

    spike using a context wrapper class to pass context instead of adding context to a property for every request type
    Change-Id: Ib6fde802d45032f1b0dfc316b870c426f417d725

--- src/TestClient/Program.cs
+++ src/TestClient/Program.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceStack.Messaging;
 using ServiceStack.Redis;
 using ServiceStack.Redis.Messaging;
 using ServiceStack.ServiceHost;
 
 namespace TestClient
 {
+	public class MessageContext<TRequest>
+	{
+		public TRequest Request { get; set; }
+		public int MessageId { get; set; }
+	}
 	public class Hello
 	{
 		public string Name { get; set; }
@@ -18,12 +24,13 @@ namespace TestClient
 		public string Result { get; set; }
 	}
 
-	public class HelloService : IService<Hello>
+	public class HelloService : IService<MessageContext<Hello>>
 	{
-		public object Execute(Hello req)
+		public object Execute(MessageContext<Hello> req)
 		{
-			Console.WriteLine("Service got a message:" + req.Name);
-			return new HelloResponse {Result = "Hello, " + req.Name};
+			Console.WriteLine("Service got a message:" + req.MessageId);
+			Console.WriteLine("Service got a message:" + req.Request.Name);
+			return new HelloResponse {Result = "Hello, " + req.Request.Name};
 		}
 	}
 
@@ -38,18 +45,23 @@ namespace TestClient
 			var redisFactory = new PooledRedisClientManager("localhost:6379");
 			var mqHost = new RedisMqHost(redisFactory);
 
-			mqHost.RegisterHandler<Hello>(m =>
-			{
-				container.Resolve<HelloService>().Execute(m.GetBody());
-				return null;
-			});
+			mqHost.RegisterHandler<MessageContext<Hello>>(m =>
+				{
+					container.Resolve<HelloService>().Execute(m.GetBody());
+					return null;
+				});
 
 			mqHost.Start(); //Starts listening for messages
 
 			var clientRedisFactory = new PooledRedisClientManager("localhost:6379");
 			var clientHost = new RedisMqHost(clientRedisFactory);
 			var mqClient = clientHost.CreateMessageQueueClient();
-			mqClient.Publish(new Hello { Name = "Client 1" });
+
+			var msg = new MessageContext<Hello>
+				{
+					MessageId = 7, Request = new Hello {Name = "Client 1"}
+				};
+			mqClient.Publish(msg);
 
 			Console.ReadLine();
 		}
